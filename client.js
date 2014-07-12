464c33bb932921563209a333bd3f296792a4b046
Matt Chisholm <matt@theory.org>
2014-07-12 13:57:31 +0200

    support PUT/POST/PATCH/DELETE

--- client.js
+++ client.js
@@ -21,7 +21,7 @@ var sign = function(auth, verb, path, params, payload, expires) {
     var to_sign = [
         verb, path,
         crypto.createHash('md5').update(serialize_params(params)).digest('hex'),
-        crypto.createHash('md5').update(payload).digest('hex'),
+        crypto.createHash('md5').update(payload || '').digest('hex'),
         auth.app_token, exp_str
     ].join('\n');
     params.sign = crypto.createHmac('sha1', auth.app_secret).update(to_sign).digest('base64');
@@ -68,15 +68,20 @@ Client.prototype.login = function (email, password, callback) {
     );
 };
 
-Client.prototype.get = function (endpoint, params, callback) {
-    sign(this.auth, 'GET', endpoint, params, '');
+Client.prototype.http = function (verb, endpoint, params, data, callback) {
+    if (typeof data === 'object') {
+        data = JSON.stringify(data);
+    }
+    sign(this.auth, verb, endpoint, params, data);
     var uri = this.uri + endpoint;
     if (Object.keys(params).length) {
         uri += '?' + serialize_params(params);
     }
     request({
         uri: uri,
-        method: 'GET'
+        method: verb,
+        headers: {"Content-Type": "application/json"},
+        body: data
     }, function (error, response, body) {
         if (error) {
             console.error(error);
@@ -84,13 +89,30 @@ Client.prototype.get = function (endpoint, params, callback) {
             console.warn(response.statusCode);
             console.warn(body);
         } else if (callback) {
-            callback(JSON.parse(body));
+            callback(verb !== 'DELETE' ? JSON.parse(body) : {});
         }
     });
 };
-Client.prototype.put = function (endpoint, params, data, callback) {};
-Client.prototype.post = function (endpoint, params, data, callback) {};
-Client.prototype.patch = function (endpoint, params, data, callback) {};
-Client.prototype.delete = function (endpoint, callback) {};
+
+Client.prototype.get = function (endpoint, params, callback) {
+    var verb = 'GET', data = '';
+    this.http(verb, endpoint, params, data, callback);
+}
+Client.prototype.put = function (endpoint, params, data, callback) {
+    var verb = 'PUT';
+    this.http(verb, endpoint, params, data, callback);
+};
+Client.prototype.post = function (endpoint, params, data, callback) {
+    var verb = 'POST';
+    this.http(verb, endpoint, params, data, callback);
+};
+Client.prototype.patch = function (endpoint, params, data, callback) {
+    var verb = 'PATCH';
+    this.http(verb, endpoint, params, data, callback);
+};
+Client.prototype.delete = function (endpoint, callback) {
+    var verb = 'DELETE', params = {}, data = '';
+    this.http(verb, endpoint, params, data, callback);
+};
 
 module.exports = Client
