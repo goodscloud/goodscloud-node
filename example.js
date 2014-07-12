81267847f584582222b6840e75a7000da350e599
Matt Chisholm <matt@theory.org>
2014-07-12 13:22:55 +0200

    example usage

---
+++ example.js
@@ -0,0 +1,11 @@
+var Client = require('./client');
+
+var c = new Client('http://sandbox.goodscloud.com');
+c.login('me@mycompany.com', 'PASSWORD',
+    function () {
+        console.info("Logged in as", c.email);
+        c.get('/api/internal/company', {flat: true}, function (data) {
+            console.info(data);
+        });
+    }
+);
